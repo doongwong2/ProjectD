7e1ad572218c635aca7220936f1ac7a7cfd27dc3
xueming28 <xuemingwang28@gmail.com>
2024-12-31 14:01:48 +0800

    add ppt

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,6 +18,7 @@ export default async function Home() {
               <p><Link href="/simplePages/memberProfiles/memberProfiles.html" className={styles.link}>MEMBER PROFILE</Link></p>
               <p><Link href="/Show Room/index.html" className={styles.link}>SHOWROOM</Link></p>
               <p><Link href="/simplePages/Admin's Guide.html" className={styles.link}>RACING GUIDE</Link></p>
+              <p><Link href="https://ntouedu-my.sharepoint.com/:p:/g/personal/01257073_o365_ntou_edu_tw/EfM2SPwGOIVCrHvddz4KaEcBIy01FDdmPIEvaEF5nfd30g?e=REQ6Dl" className={styles.link}>SLIDE SHOW</Link></p>
               <p><Link href="/simplePages/credits.html" className={styles.link}>CREDITS</Link></p>
             </div>
             <h1 className={styles.header1}>PROJECT.D</h1>
